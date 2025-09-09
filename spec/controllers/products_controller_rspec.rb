9e0d5184ac0e9e8c093f19bbb1069718b0560be3
omprakash <omprakashkurmi205@gmail.com>
2025-09-09 14:58:50 +0530

    project setup

---
+++ spec/controllers/products_controller_rspec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe ProductsController, type: :controller do
+  describe "GET#Index" do
+    context 'write test cases of index action' do
+
+     it '#GET index found all data'do
+      product = Product.create(name: "tea", price: 102.2, stock: 2)
+      expect(product['name']).to eq(product.name)
+     end
+    end 
+  end
+end
