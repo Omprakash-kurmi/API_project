dc2ef34e7341f54b6f363740612d1d94e0f9234a
omprakash <omprakashkurmi205@gmail.com>
2025-09-12 17:30:05 +0530

    add validation in account model

--- spec/factories/account_block/accounts.rb
+++ spec/factories/account_block/accounts.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
-  factory :account_block_account, class: 'AccountBlock::Account' do
-    
+  factory :account, class: 'AccountBlock::Account' do
+    first_name {Faker::Name.first_name }
+    last_name {Faker::Name.last_name }
+    email {Faker::Internet.unique.email}
+    association :role, factory: :role 
+    full_phone_number {"918765432189"}
+    password   { "password123" }
   end
 end
